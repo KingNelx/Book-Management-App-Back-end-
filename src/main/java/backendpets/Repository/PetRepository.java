0bf0b266fa1132e126d94e9825135212c2f144b5
KingNelx <tapiajonel606@gmail.com>
2023-02-18 10:35:07 +0800

    new environment

--- src/main/java/backendpets/Repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package backendpets.Repository;
-
-import backendpets.Model.Pets;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface PetRepository extends JpaRepository <Pets, Long> {
-    List<Pets> findAllByTypeOfPet(String typeOfPet);
-    List<Pets> findAllByOwnerName(String ownerName);
-}
