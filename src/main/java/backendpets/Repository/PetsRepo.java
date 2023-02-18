3a9055d80cae51027bee3c25048615709f1ead04
KingNelx <tapiajonel606@gmail.com>
2023-02-18 18:16:27 +0800

    ulit

--- src/main/java/backendpets/Repository/PetsRepo.java
+++
@@ -1,9 +0,0 @@
-package backendpets.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import backendpets.Model.Pets;
-
-public interface PetsRepo extends JpaRepository <Pets, Long>{
-    
-}
