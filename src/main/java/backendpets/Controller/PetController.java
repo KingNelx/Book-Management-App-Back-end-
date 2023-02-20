07f7b5aeee56197e543ce0bd9d7ac6de8d2950a6
KingNelx <tapiajonel606@gmail.com>
2023-02-20 09:23:32 +0800

    added request methods in cross origin

--- src/main/java/backendpets/Controller/PetController.java
+++ src/main/java/backendpets/Controller/PetController.java
@@ -3,6 +3,7 @@ package backendpets.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +11,11 @@ import javax.validation.Valid;
 import backendpets.Model.Pets;
 import backendpets.Repository.PetRepository;
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "http://localhost:517", methods = {RequestMethod.GET, RequestMethod.POST}, allowedHeaders = "*")
 public class PetController{
 
     @Autowired
