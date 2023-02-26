f70a962a21dfa85bbad8b4e765d40fe50de0d1a3
KingNelx <tapiajonel606@gmail.com>
2023-02-26 18:05:18 +0800

    added time stamp

--- src/main/java/backendpets/Exception/PetErrorAdvice.java
+++ src/main/java/backendpets/Exception/PetErrorAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +20,7 @@ public class PetErrorAdvice {
     public Map <String, String> exceptionHandler(PetNotFound exceNotFound){
         Map <String, String> errorMap = new HashMap<>();
         errorMap.put("ERROR MESSAGE", exceNotFound.getMessage());
+        errorMap.put("timestamp", LocalDateTime.now().toString());
         return errorMap;
     } 
 }
