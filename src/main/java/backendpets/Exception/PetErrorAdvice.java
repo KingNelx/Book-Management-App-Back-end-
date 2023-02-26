e0879acd22152aef8f4326c912a9029c945b9904
KingNelx <tapiajonel606@gmail.com>
2023-02-26 17:59:20 +0800

    added response status

--- src/main/java/backendpets/Exception/PetErrorAdvice.java
+++ src/main/java/backendpets/Exception/PetErrorAdvice.java
@@ -1,8 +1,10 @@
 package backendpets.Exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +15,7 @@ public class PetErrorAdvice {
 
     @ResponseBody
     @ExceptionHandler(PetNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map <String, String> exceptionHandler(PetNotFound exceNotFound){
         Map <String, String> errorMap = new HashMap<>();
         errorMap.put("ERROR MESSAGE", exceNotFound.getMessage());
