521b25ca7aacbd030c861d562347e7a5a38f91bc
KingNelx <tapiajonel606@gmail.com>
2023-03-08 12:33:34 +0800

    added relationships

--- src/main/java/backendpets/Model/Owner.java
+++ src/main/java/backendpets/Model/Owner.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
