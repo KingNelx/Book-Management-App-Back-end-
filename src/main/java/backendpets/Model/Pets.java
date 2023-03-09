1710fd448398980c506c1753c8e24f811d925a6c
KingNelx <tapiajonel606@gmail.com>
2023-03-10 05:50:02 +0800

    added jpa query

--- src/main/java/backendpets/Model/Pets.java
+++ src/main/java/backendpets/Model/Pets.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -15,10 +16,19 @@ import javax.validation.constraints.NotEmpty;
 @NoArgsConstructor
 @Table(name = "pets_table")
 @Entity
+@NotBlank
+@NotEmpty
 public class Pets {
     
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
+    private String ownerName;
+    private String petName;
+    private int ownerAge;
+    private int petAge;
+    private String address;
+    private String typeOfPet;
+    private String hasVaccine;
 }
--- src/main/java/backendpets/Repository/PetsRepo.java
+++ src/main/java/backendpets/Repository/PetsRepo.java
@@ -1,5 +1,10 @@
 package backendpets.Repository;
 
-public interface PetsRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import backendpets.Model.Pets;
+
+public interface PetsRepo extends JpaRepository<Pets, Long>{
+    List<Pets> findAllByTypeOfPet(String typeOfPet);
     
 }
