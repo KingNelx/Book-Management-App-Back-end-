4adff9d6b5a2a68f208ddf48665c8c0d620b9279
KingNelx <tapiajonel606@gmail.com>
2023-03-10 08:35:29 +0800

    added rest controller

--- src/main/java/backendpets/Controller/PetController.java
+++ src/main/java/backendpets/Controller/PetController.java
@@ -1,5 +1,21 @@
 package backendpets.Controller;
 
+import backendpets.Repository.PetsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class PetController {
     
+    @Autowired
+    private PetsRepo petsRepo;
+
+    @PostMapping
 }
