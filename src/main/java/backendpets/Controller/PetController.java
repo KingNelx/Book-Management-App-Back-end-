d752bc7a5948ea91f1f5386a709f2d8da45ca462
KingNelx <tapiajonel606@gmail.com>
2023-03-11 22:02:41 +0800

    remove uneccesary imports

--- src/main/java/backendpets/Controller/PetController.java
+++ src/main/java/backendpets/Controller/PetController.java
@@ -3,11 +3,9 @@ package backendpets.Controller;
 import backendpets.Model.Pets;
 import backendpets.Repository.PetsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.server.ResponseStatusException;
+
 
 import java.util.List;
 
