c4ca46fb6a63e182ff3d354452ecb296cc04a971
KingNelx <tapiajonel606@gmail.com>
2023-03-26 21:07:21 +0800

    added fields in my owner model

--- src/main/java/backendpets/Model/Owner.java
+++ src/main/java/backendpets/Model/Owner.java
@@ -1,5 +1,17 @@
 package backendpets.Model;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
 public class Owner {
     
 }
