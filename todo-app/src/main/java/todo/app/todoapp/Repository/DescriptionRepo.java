29acbbb49cd05cd5e8fe7d6bc7f9a055826bd651
KingNelx <tapiajonel606@gmail.com>
2023-04-16 13:52:17 +0800

    reset

--- todo-app/src/main/java/todo/app/todoapp/Repository/DescriptionRepo.java
+++
@@ -1,8 +0,0 @@
-package todo.app.todoapp.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import todo.app.todoapp.Model.Description;
-
-public interface DescriptionRepo extends MongoRepository<Description, String> {
-}
--- todo-app/src/main/java/todo/app/todoapp/Repository/TasksRepo.java
+++
@@ -1,9 +0,0 @@
-package todo.app.todoapp.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import todo.app.todoapp.Model.Tasks;
-
-public interface TasksRepo extends MongoRepository<Tasks, String> {
-
-}
