b5c4bc5c3b6678c36aec9d8bb68def2a91ec4499
KingNelx <tapiajonel606@gmail.com>
2023-04-18 23:22:36 +0800

    added rest api for reviews

--- Book-App/src/main/java/book/app/BookApp/Controller/AuthorController.java
+++ Book-App/src/main/java/book/app/BookApp/Controller/AuthorController.java
@@ -1,10 +1,12 @@
 package book.app.BookApp.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class AuthorController {
     @GetMapping("/getAuthors")
     public List <Author> getAllAuthors(){
         return authorService.getAuthors();
+    }
+
+    @GetMapping("/getAuthorByID/{id}")
+    public Optional <Author> getAuthorByID(@PathVariable String id){
+        return authorService.getAuthorByID(id);
     }
 }
--- Book-App/src/main/java/book/app/BookApp/Controller/ReviewController.java
+++ Book-App/src/main/java/book/app/BookApp/Controller/ReviewController.java
@@ -3,6 +3,7 @@ package book.app.BookApp.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import book.app.BookApp.Model.Reviews;
 import book.app.BookApp.Service.ReviewService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/reviews")
@@ -22,5 +24,10 @@ public class ReviewController {
     @GetMapping("/getAllReviews")
     public List <Reviews> getAllReviews(){
         return reviewService.getAllReviews();
+    }    
+
+    @GetMapping("/getReviewsByID/{id}")
+    public Optional <Reviews> getReviewsByID(@PathVariable String id){
+        return reviewService.getReviewsByID(id);
     }
 }
